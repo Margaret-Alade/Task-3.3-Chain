4f1ed57d343dab95d1cde49de6c562a5da04f3dd
Margaret Alade <alade.margaret@yandex.ru>
2024-08-12 06:49:02 +0300

    Initial Commit

---
+++ Task-3.3-Chain/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-3.3-Chain
+//
+//  Created by Маргарет  on 12.08.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
